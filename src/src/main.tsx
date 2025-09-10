2a09132c016b50b9257fe3b7c9481c9aa42edf08
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2025-09-10 02:10:55 +0200

    x

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx';
-import StandardizeTitolari from './StandardizeTitolari';
-import Navbar from './Navbar';
+// import StandardizeTitolari from './StandardizeTitolari';
+// import Navbar from './Navbar';
 
-const url = window.location.pathname;
-const isStandardPage = url.includes('standardize');
+// const url = window.location.pathname;
+// const isStandardPage = url.includes('standardize');
 
 function MainRouter() {
   return (
